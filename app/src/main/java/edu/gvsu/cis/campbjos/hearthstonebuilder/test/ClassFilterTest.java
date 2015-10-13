e6e3500eb7289396b0d0f4f596d709455541fb63
teitsmch <teitsmch@mail.gvsu.edu>
2015-10-12 21:31:48 -0400

    Completed unit testing

--- app/src/main/java/edu/gvsu/cis/campbjos/hearthstonebuilder/test/ClassFilterTest.java
+++ app/src/main/java/edu/gvsu/cis/campbjos/hearthstonebuilder/test/ClassFilterTest.java
@@ -36,5 +36,95 @@ public class ClassFilterTest extends InstrumentationTestCase {
         }
     }
 
+    public void testCostFilter1() {
+        ArrayList<Card> fakeCards = new ArrayList<Card>();
+        String response = null;
+        try {
+            response = NetworkUtil.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1", "X-Mashape-Key", "bexJwbUuYPmshc9PHODsSl9ToBo6p1qjKxojsn2mTw1utrOlqo");
+            JsonUtil.parse(response, fakeCards);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Card> actual = CardFilter.filterCards(fakeCards, "CLEAR", "6", "CLEAR", "CLEAR", "CLEAR");
+        for (int i = 0; i < actual.size(); i++){
+            assertEquals(actual.get(i).getCost(), 6);
+        }
+    }
+
+    public void testCostFilter2() {
+        ArrayList<Card> fakeCards = new ArrayList<Card>();
+        String response = null;
+        try {
+            response = NetworkUtil.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1", "X-Mashape-Key", "bexJwbUuYPmshc9PHODsSl9ToBo6p1qjKxojsn2mTw1utrOlqo");
+            JsonUtil.parse(response, fakeCards);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Card> actual = CardFilter.filterCards(fakeCards, "CLEAR", "7+", "CLEAR", "CLEAR", "CLEAR");
+        for (int i = 0; i < actual.size(); i++){
+            assertFalse(actual.get(i).getCost() < 7);
+        }
+    }
+
+    public void testTypeFilter() {
+        ArrayList<Card> fakeCards = new ArrayList<Card>();
+        String response = null;
+        try {
+            response = NetworkUtil.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1", "X-Mashape-Key", "bexJwbUuYPmshc9PHODsSl9ToBo6p1qjKxojsn2mTw1utrOlqo");
+            JsonUtil.parse(response, fakeCards);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Card> actual = CardFilter.filterCards(fakeCards, "CLEAR", "CLEAR", "Minion", "CLEAR", "CLEAR");
+        for (int i = 0; i < actual.size(); i++){
+            assertEquals(actual.get(i).getType(), "Minion");
+        }
+    }
+
+    public void testRarityFilter() {
+        ArrayList<Card> fakeCards = new ArrayList<Card>();
+        String response = null;
+        try {
+            response = NetworkUtil.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1", "X-Mashape-Key", "bexJwbUuYPmshc9PHODsSl9ToBo6p1qjKxojsn2mTw1utrOlqo");
+            JsonUtil.parse(response, fakeCards);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Card> actual = CardFilter.filterCards(fakeCards, "CLEAR", "CLEAR", "CLEAR", "Rare", "CLEAR");
+        for (int i = 0; i < actual.size(); i++){
+            assertEquals(actual.get(i).getRarity(), "Rare");
+        }
+    }
+
+    public void testSetFilter() {
+        ArrayList<Card> fakeCards = new ArrayList<Card>();
+        String response = null;
+        try {
+            response = NetworkUtil.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1", "X-Mashape-Key", "bexJwbUuYPmshc9PHODsSl9ToBo6p1qjKxojsn2mTw1utrOlqo");
+            JsonUtil.parse(response, fakeCards);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Card> actual = CardFilter.filterCards(fakeCards, "CLEAR", "CLEAR", "CLEAR", "Clear", "Classic");
+        for (int i = 0; i < actual.size(); i++){
+            assertFalse(!(actual.get(i).getCardSet().equals("Basic") || actual.get(i).getCardSet().equals("Classic")));
+        }
+    }
+
 
 }

5a8a4d89bda5b89b8f8cef27c77c2e430097a3bf
Nathan Allvin <nathana9722@gmail.com>
2015-10-30 14:00:06 -0400

    Started some detail activity stuff

--- app/src/main/java/edu/gvsu/cis/campbjos/hearthstonebuilder/DetailActivity.java
+++ app/src/main/java/edu/gvsu/cis/campbjos/hearthstonebuilder/DetailActivity.java
@@ -1,7 +1,9 @@
 package edu.gvsu.cis.campbjos.hearthstonebuilder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,20 +11,34 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
+import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class DetailActivity extends AppCompatActivity {
 
   @InjectView(R.id.toolbar)
   Toolbar toolbar;
+  @InjectView(R.id.cardView)
+  ImageView cardImage;
+  @InjectView(R.id.toolbar_layout)
+  CollapsingToolbarLayout cToolLayout;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_detail);
-
+    ButterKnife.inject(this);
     setSupportActionBar(toolbar);
+
+    Intent intent = getIntent();
+    String imageURL = intent.getStringExtra("card");
+    String cardName = intent.getStringExtra("name");
+    cToolLayout.setTitle(cardName);
+    Picasso.with(this).load(imageURL).into(cardImage);
   }
 
   @Override

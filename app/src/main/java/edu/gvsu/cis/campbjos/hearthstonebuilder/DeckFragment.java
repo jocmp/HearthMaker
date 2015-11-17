20f7a9e22c07852f0e039e2ffc4be7ecfda6342e
Josiah Campbell <campbjos@mail.gvsu.edu>
2015-11-17 12:43:06 -0500

    Fix list generation

--- app/src/main/java/edu/gvsu/cis/campbjos/hearthstonebuilder/DeckFragment.java
+++ app/src/main/java/edu/gvsu/cis/campbjos/hearthstonebuilder/DeckFragment.java
@@ -103,6 +103,8 @@ public class DeckFragment extends Fragment {
     return fragment;
   }
 
+
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);

8c32239ec91def0a67955ca058161d9e394350af
teitsmch <teitsmch@mail.gvsu.edu>
2015-12-07 10:43:59 -0500

    added placeholders for gold weapon/spell/minion types, changed rename deck positive action to confirm rather than create

--- app/src/main/java/edu/gvsu/cis/campbjos/hearthstonebuilder/DetailActivity.java
+++ app/src/main/java/edu/gvsu/cis/campbjos/hearthstonebuilder/DetailActivity.java
@@ -1,13 +1,5 @@
 package edu.gvsu.cis.campbjos.hearthstonebuilder;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.stream.JsonWriter;
-
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -19,7 +11,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,31 +20,24 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import com.squareup.picasso.Picasso;
 
-import edu.gvsu.cis.campbjos.hearthstonebuilder.Entity.Card;
-import edu.gvsu.cis.campbjos.hearthstonebuilder.Entity.Deck;
-import edu.gvsu.cis.campbjos.hearthstonebuilder.UI.CardIconCrop;
-
-import org.json.JSONObject;
-
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import butterknife.OnClick;
+import edu.gvsu.cis.campbjos.hearthstonebuilder.Entity.Card;
+import edu.gvsu.cis.campbjos.hearthstonebuilder.UI.CardIconCrop;
 
 
 public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
@@ -183,6 +167,7 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
       cardText = "No Card Text";
     }
 
+    //set expanded color to transparent
     mToolbarLayout.setExpandedTitleColor(Color.parseColor("#00FFFFFF"));
 
     rarityDetail.setText(cardRarity);
@@ -198,13 +183,34 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
     setDetail.setText(cardSet);
     manaDetail.setText(String.valueOf(cardMana));
 
-    Glide.with(this)
-            .load(goldURL)
-            .thumbnail(0.1f)
-            //.placeholder(R.drawable.placeholder_gold)
-            .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-            .into(gifImage);
+    //load gif into the gold image view
+    switch (cardType) {
+      case "Spell":
+      Glide.with(this)
+              .load(goldURL)
+              .placeholder(R.drawable.placeholder_gold_spell)
+              .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+              .into(gifImage);
+        break;
+      case "Minion":
+        Glide.with(this)
+                .load(goldURL)
+                .placeholder(R.drawable.placeholder_gold_minion)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(gifImage);
+        break;
+      case "Weapon":
+        Glide.with(this)
+                .load(goldURL)
+                .placeholder(R.drawable.placeholder_gold_weapon)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(gifImage);
+        break;
+    }
+
+    //load into normal card image view
     Picasso.with(this).load(imageURL).into(cardImage);
+    //load into the icon
     Picasso.with(this).load(imageURL).transform(CardIconCrop.getCardIconCrop()).into(cardIcon);
 
     switch (cardClass) {
